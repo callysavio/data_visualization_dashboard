b36355567ed14efbea2abe175bafdcd8ed759b7a
Callistus Anwara <callistusanwara@gmail.com>
2024-01-25 15:26:38 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ if (NODE_ENV === "production") {
   app.use(morgan("dev"));
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')))
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'))
